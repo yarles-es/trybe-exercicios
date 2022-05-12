ab48daacdf18532c1e9a30d86e6d8a97fd9c4d52
Yarles de Andrade Espirito Santo <yarles@pop-os.localdomain>
2022-05-12 18:35:36 -0300

    realizado exercicio 1º e 2º da materia JavaScript

--- fundamentos/introdução_javascript/dia_01_introdução_javascript/exercicio_1º_do_dia_4.1_do_curso/scripts.js
+++ fundamentos/introdução_javascript/dia_01_introdução_javascript/exercicio_1º_do_dia_4.1_do_curso/scripts.js
@@ -16,5 +16,3 @@ birthYear = 2030;
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
-
-/* exercicio realizado*/
---
+++ fundamentos/introdução_javascript/dia_01_introdução_javascript/exercicio_2º_do_dia_4.1_do_curso/scripts.js
@@ -0,0 +1,10 @@
+/* Crie uma constante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.*/
+
+const base = 5;
+let height = 8;
+const area = (base * height);
+console.log(area);
+const perimeter = base * 2 + height * 2 ;
+console.log(perimeter);
