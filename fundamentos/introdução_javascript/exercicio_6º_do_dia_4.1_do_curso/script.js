b77b72a3f5f811665e5d55c0b7a0835000fcc9a1
Yarles de Andrade Espirito Santo <yarles@pop-os.localdomain>
2022-05-12 19:23:22 -0300

    realizado exercicio 4º 5° 6º dia 4.1 de JavaScript

---
+++ fundamentos/introdução_javascript/exercicio_6º_do_dia_4.1_do_curso/script.js
@@ -0,0 +1,14 @@
+let qualificacao = "lista";
+
+switch (qualificacao) {
+    case 'aprovada' , 'lista':
+        console.log("FINALMENTE, descanso merecido UwU");
+        break;
+    
+        case 'reprovada':
+        console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+        break;
+
+        default:
+            console.log('não se aplica');
+}
